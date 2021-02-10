f807705e27f2885e4d4e02b05a930239e7bb469c
Ze2QvoQxxKeu <17485135+Ze2QvoQxxKeu@users.noreply.github.com>
2021-02-11 00:58:33 +0400

    Версия 1.0.0.0

---
+++ DatalessonMain.xaml.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Navigation;
+using System.Net;
+using System.Net.Http;
+using System.IO;
+using System.Diagnostics;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace DatalessonCertificate
+{
+    public partial class DatalessonMain : Window
+    {
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
+        }
+
+        public DatalessonMain()
+        {
+            InitializeComponent();
+        }
+
+        private async void Окно_Loaded(object sender, RoutedEventArgs e)
+        {
+            using (new WaitCursor())
+            {
+                ИмяПользователя.Text = string.IsNullOrWhiteSpace(Properties.Settings.Default.UserName) ? Environment.UserName : Properties.Settings.Default.UserName;
+                Сайт.NavigateUri = new Uri(СайтПрограммы);
+                СайтПрограммыХинт.Text = СайтПрограммы;
+                Значок.Source = IconToImageSource(Properties.Resources.datalesson);
+                string error = string.Empty;
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11;
+
+                await Task.Run(() =>
+                {
+                    foreach (var Файл in ФайлКонфигурации)
+                    {
+                        Dispatcher.Invoke(() => НазваниеУрока.Items.Clear());
+                        using (WebClient client = new WebClient())
+                            try
+                            {
+                                using (StreamReader reader = new StreamReader(client.OpenRead(Файл)))
+                                    while (reader.Peek() >= 0)
+                                    {
+                                        string[] item = reader.ReadLineAsync().Result.Split('|');
+                                        if (item.Length == 2 || item.Length == 5)
+                                        {
+                                            Уроки.Add(new ДанныеУрока(item));
+                                            Dispatcher.Invoke(() => НазваниеУрока.Items.Add(item[0]));
+                                        }
+                                    }
+                            }
+                            catch (WebException ex)
+                            {
+                                error = ex.Message;
+                            }
+                            catch (Exception ex)
+                            {
+                                error = ex.Message;
+                            }
+                        if (string.IsNullOrEmpty(error))
+                            break;
+                        else
+                            error = string.Empty;
+                    }
+                });
+                if (НазваниеУрока.Items.Count == 0 || !string.IsNullOrEmpty(error))
+                {
+                    НазваниеУрока.Items.Clear();
+                    Уроки.Clear();
+                    MessageBox.Show($"Ошибка загрузки файла конфигурации.\n{error}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                    НазваниеУрока.SelectedIndex = НазваниеУрока.Items.Count - 1;
+            }
+        }
+
+        private async void ПолучитьСертификат_Click(object sender, RoutedEventArgs e)
+        {
+            ПолучитьСертификат.IsEnabled = false;
+            using (new WaitCursor())
+                try
+                {
+                    if (НазваниеУрока.SelectedIndex == -1)
+                    {
+                        MessageBox.Show("Список уроков не загружен.\nЗакройте программу и повторите попытку позднее.", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                        return;
+                    }
+                    if (string.IsNullOrWhiteSpace(ИмяПользователя.Text) && НазваниеУрока.SelectedIndex < НовыйАлгоритм)
+                    {
+                        MessageBox.Show("Введите имя и повторите попытку.", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                        return;
+                    }
+                    string error_text = string.Empty;
+                    try
+                    {
+                        //Константы
+                        const string MissingData = "Запрошенная страница не содержит искомых данных.\nПроверьте обновление программы.";
+                        const string GettingIdError = "Ошибка получения нового идентификатора пользователя.\nПопробуйте повторить попытку позднее.";
+                        const string SiteUnavailable = "Сайт \"{0}\" недоступен.\nПроверьте подключение к интернет или попробуйте повторить попытку позднее.";
+                        const string SiteTemporaryUnavailable = "Сайт \"{0}\" временно недоступен.\nПопробуйте повторить попытку позднее.";
+
+                        string user_id = string.Empty;
+                        CookieContainer cookies = new CookieContainer();
+                        HttpClientHandler handler = new HttpClientHandler
+                        {
+                            CookieContainer = cookies,
+                            AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
+                        };
+                        HttpClient client = new HttpClient(handler);
+                        client.DefaultRequestHeaders.Add(HttpRequestHeader.UserAgent.ToString(), UserAgent);
+                        HttpResponseMessage response;
+
+                        if (НазваниеУрока.SelectedIndex < НовыйАлгоритм)
+                        {
+                            //Получаем новый user_id
+                            response = await client.PostAsync("https://form.datalesson.ru/api/v1/auth",
+                                                                    new StringContent(RandLoginRequest(), Encoding.UTF8, "application/json"));
+                            if (!response.IsSuccessStatusCode)
+                                throw new Exception(string.Format(SiteUnavailable, "form.datalesson.ru"));
+
+                            //Обработка ответа
+                            LoginResponse loginResponse = JsonConvert.DeserializeObject<LoginResponse>(await response.Content.ReadAsStringAsync());
+                            if (loginResponse.data != null)
+                                user_id = loginResponse.data.user_id;
+                            else if (loginResponse.error != null)
+                                throw new Exception($"{loginResponse.error.code}: {loginResponse.error.descrption}");
+                            else
+                                throw new Exception(GettingIdError);
+                            if (new Regex(@"[a-f\d]{8}-[a-f\d]{4}-[a-f\d]{4}-[a-f\d]{4}-[a-f\d]{12}", RegexOptions.IgnoreCase).IsMatch(user_id))
+                            {
+                                //Загрузка сертификата
+                                response = await client.GetAsync("https://form.datalesson.ru/api/v1/certificates/student/" +
+                                    $"{user_id}?challenge_type={Уроки[НазваниеУрока.SelectedIndex].Тренажер}&name={ИмяПользователя.Text}");
+                                if (!response.IsSuccessStatusCode)
+                                    throw new Exception("Ошибка получения сертификата. Повторите попытку позднее.");
+                                else
+                                {
+                                    string FileName = $"{Уроки[НазваниеУрока.SelectedIndex].Название}.pdf";
+                                    if (GetSaveFileName(ref FileName))
+                                    {
+                                        using (var ms = await response.Content.ReadAsStreamAsync())
+                                        using (var fs = File.Create(FileName))
+                                        {
+                                            ms.Seek(0, SeekOrigin.Begin);
+                                            ms.CopyTo(fs);
+                                        }
+                                        MessageBox.Show("Сертификат успешно сохранён.", "Информация", MessageBoxButton.OK, MessageBoxImage.Information);
+                                    }
+                                    else
+                                        MessageBox.Show("Сохранение сертификата отменено.", "Информация", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                                }
+                            }
+                            else
+                                throw new Exception(MissingData);
+                        }
+                        else
+                        {
+                            //Грузим главную страницу. Получаем куки, парсим данные
+                            response = await client.GetAsync($"https://xn--h1adlhdnlo2c.xn--p1ai/?rnd={rand.Next()}");
+                            if (!response.IsSuccessStatusCode)
+                                throw new Exception(string.Format(SiteUnavailable, "урокцифры.рф"));
+
+                            string buffer = await response.Content.ReadAsStringAsync();
+
+                            //Переменные
+                            string _token = string.Empty;
+                            List<int> regions = new List<int>();
+
+                            //Парсим токен
+                            Match match = new Regex(@"name=_token\s+value=([A-z\d]{40})").Match(buffer);
+                            if (match.Success)
+                            {
+                                _token = match.Groups[1].Value;
+
+                                //Парсим регионы
+                                int startIndex = buffer.IndexOf("<select name=id_region");
+                                if (startIndex == -1)
+                                    throw new Exception(MissingData);
+                                int length = buffer.Substring(startIndex).IndexOf("</select>");
+                                if (length == -1)
+                                    throw new Exception(MissingData);
+                                if (!ParseList(buffer.Substring(startIndex, length), ref regions))
+                                    throw new Exception(MissingData);
+                            }
+                            else
+                                throw new Exception(MissingData);
+
+                            //Переменные
+                            List<int> cities = new List<int>(), countries = new List<int>();
+                            bool not_from_russia = Convert.ToBoolean(rand.Next(2));
+                            int id_region = RandomListValue(regions), id_city = 0, id_country = 0;
+
+                            if (not_from_russia) //Если не из рашки
+                            {
+                                //Парсим страны
+                                response = await client.PostAsync($"https://xn--h1adlhdnlo2c.xn--p1ai/load-countries?rnd={rand.Next()}",
+                                                                    new StringContent(string.Join("&", new Dictionary<string, string>
+                                                                    {
+                                                                    { "_token", _token },
+                                                                    }.Select(x => x.Key + "=" + x.Value).ToArray()),
+                                                                        Encoding.UTF8, "application/x-www-form-urlencoded"));
+                                if (!response.IsSuccessStatusCode)
+                                    throw new Exception(string.Format(SiteTemporaryUnavailable, "урокцифры.рф"));
+                                if (!ParseList(await response.Content.ReadAsStringAsync(), ref countries))
+                                    throw new Exception(MissingData);
+                                id_country = RandomListValue(countries);
+                            }
+                            else //Из рашки
+                            {
+                                //Парсим города
+                                response = await client.PostAsync($"https://xn--h1adlhdnlo2c.xn--p1ai/load-region-cities?rnd={rand.Next()}",
+                                                                    new StringContent(string.Join("&", new Dictionary<string, string>
+                                                                    {
+                                                                    { "id", id_region.ToString() },
+                                                                    { "_token", _token },
+                                                                    }.Select(x => x.Key + "=" + x.Value).ToArray()),
+                                                                        Encoding.UTF8, "application/x-www-form-urlencoded"));
+                                if (!response.IsSuccessStatusCode)
+                                    throw new Exception(string.Format(SiteTemporaryUnavailable, "урокцифры.рф"));
+                                if (!ParseList(await response.Content.ReadAsStringAsync(), ref cities))
+                                    throw new Exception(MissingData);
+                                id_city = RandomListValue(cities);
+                            }
+
+                            //Переменные
+                            string type = new string[] { "pupil", "parent", "teacher" }[rand.Next(3)];
+                            int age_id = rand.Next(3); //тренажёр[1-4][5-7][8-11]
+
+                            //Получаем новый user_id
+                            response = await client.PostAsync("https://xn--h1adlhdnlo2c.xn--p1ai/trainer/send-request",
+                                                                new StringContent(string.Join("&", new Dictionary<string, string>
+                                                                {
+                                                                { "type", type
+                                                                + (not_from_russia ? "&not_from_russia=on" : string.Empty) },
+                                                                { "id_country", id_country.ToString() },
+                                                                { "id_region", not_from_russia ? string.Empty : id_region.ToString() },
+                                                                { "id_city", not_from_russia ? string.Empty : id_city.ToString() },
+                                                                { "grade", type == "teacher" ? string.Empty : RandomGradeByAgeId(age_id).ToString() },
+                                                                { "pass_type", "self" },
+                                                                { "id_trainer", Уроки[НазваниеУрока.SelectedIndex].Тренажеры[age_id].ToString() },
+                                                                { "id_lesson", Уроки[НазваниеУрока.SelectedIndex].Урок.ToString() },
+                                                                { "_token", _token },
+                                                                { "passType", "self" },
+                                                                }.Select(x => x.Key + "=" + x.Value).ToArray()),
+                                                                    Encoding.UTF8, "application/x-www-form-urlencoded"));
+                            if (!response.IsSuccessStatusCode)
+                                throw new Exception(GettingIdError);
+
+                            //Обработка ответа
+                            SendRequestResponse srr = JsonConvert.DeserializeObject<SendRequestResponse>(await response.Content.ReadAsStringAsync());
+                            if (srr.status)
+                            {
+                                match = new Regex(@"[a-f\d]{8}-[a-f\d]{4}-[a-f\d]{4}-[a-f\d]{4}-[a-f\d]{12}", RegexOptions.IgnoreCase).Match(srr.url);
+                                if (match.Success)
+                                {
+                                    user_id = match.Value;
+
+                                    MessageBox.Show("Страница выдачи сертификата будет открыта в браузере.", "Информация",
+                                                    MessageBoxButton.OK, MessageBoxImage.Information);
+                                    //Открытие страница выдачи сертификата в браузере
+                                    Process.Start(Уроки[НазваниеУрока.SelectedIndex].Ссылка.Replace("{user_id}", user_id));
+                                }
+                            }
+                        }
+                    }
+                    catch (WebException ex)
+                    {
+                        error_text = ex.Message;
+                    }
+                    catch (Exception ex)
+                    {
+                        error_text = ex.Message;
+                    }
+                    if (!string.IsNullOrEmpty(error_text))
+                        MessageBox.Show($"Ошибка получения сертификата.\n{error_text}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                finally
+                {
+                    ПолучитьСертификат.IsEnabled = true;
+                }
+        }
+
+        private void Закрыть_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Свернуть_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void ЗаголовокОкна_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void Инфо_Click(object sender, RoutedEventArgs e)
+        {
+            DatalessonAbout aboutForm = new DatalessonAbout();
+            aboutForm.Owner = this;
+            aboutForm.ShowDialog();
+        }
+
+        private void MainDlg_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Properties.Settings.Default.UserName = ИмяПользователя.Text.Trim().Equals(Environment.UserName) ? string.Empty : ИмяПользователя.Text.Trim();
+            Properties.Settings.Default.Save();
+        }
+    }
+}
---
+++ DatalessonMainUtils.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using Microsoft.Win32;
+using Newtonsoft.Json;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+using System.Reflection;
+using System.Windows.Interop;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace DatalessonCertificate
+{
+    public partial class DatalessonMain : Window
+    {
+        private static readonly List<string> ФайлКонфигурации = new List<string>
+        {
+            @"https://raw.githubusercontent.com/Ze2QvoQxxKeu/DatalessonCertificate/main/[CONFIG]",
+            @"https://pastebin.com/raw/hKCZjndf"
+        };
+        public static readonly string СайтПрограммы = @"https://github.com/Ze2QvoQxxKeu/DatalessonCertificate";
+        public static readonly string УрокЦифры = @"https://урокцифры.рф/";
+        private static string UserAgent
+        {
+            get
+            {
+                Match match = new Regex(@"^([^,]+),\s*Version=([^,]+)").Match(Assembly.GetExecutingAssembly().FullName);
+                if (match.Success)
+                    return $"{match.Groups[1].Value}/{match.Groups[2].Value}";
+                else
+                    return $"DatalessonCertificate/{FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVersion}";
+            }
+        }
+        private readonly List<ДанныеУрока> Уроки = new List<ДанныеУрока>();
+        private static readonly Random rand = new Random();
+        private static readonly int НовыйАлгоритм = 5;
+
+        private struct ДанныеУрока
+        {
+            public string Название;
+            public string Тренажер;
+            public int Урок;
+            public int[] Тренажеры;
+            public string Ссылка;
+            public ДанныеУрока(string[] Данные)
+            {
+                this.Название = Данные[0];
+                this.Тренажер = Данные[1];
+                this.Урок = Данные.Length == 5 ? int.Parse(Данные[2]) : 0;
+                this.Тренажеры = Данные.Length == 5 ? Array.ConvertAll(Данные[3].Split(','), int.Parse) : null;
+                this.Ссылка = Данные.Length == 5 ? Данные[4] : string.Empty;
+            }
+        }
+
+        private string RandLoginRequest()
+        {
+            LoginRequest loginData = new LoginRequest
+            {
+                age_group = rand.Next(1, 3),
+                type = new string[] { "student", "teacher", "parent" }[rand.Next(3)],
+                region = RandomString(rand.Next(6, 12)),
+                state = RandomString(rand.Next(6, 12)),
+                vk_id = null,
+                trainer = Уроки[НазваниеУрока.SelectedIndex].Тренажер,
+                class_no = rand.Next(1, 11),
+                school = rand.Next(1, 2000).ToString(),
+                discipline = new List<string> { },
+                children_ages = new List<int>
+                {
+                    rand.Next(7, 17)
+                },
+                student_info = new List<StudentInfo>
+                {
+                    new StudentInfo
+                    {
+                        age = rand.Next(7, 17),
+                        sex = new string[] { "male", "female" }[rand.Next(2)]
+                    }
+                }
+            };
+            int count = rand.Next(4);
+            while (count-- > 0)
+                loginData.discipline.Add(RandomString(rand.Next(6, 12)));
+            return JsonConvert.SerializeObject(loginData);
+        }
+
+        private static string RandomString(int length = 10)
+        {
+            const string chars = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+            return (new CultureInfo("ru-RU", false).TextInfo).ToTitleCase(new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[rand.Next(s.Length)]).ToArray()));
+        }
+
+        private class StudentInfo
+        {
+            public int age { get; set; }
+            public string sex { get; set; }
+        }
+
+        private class LoginRequest
+        {
+            public int age_group { get; set; }
+            public string type { get; set; }
+            public string region { get; set; }
+            public string state { get; set; }
+            public int? vk_id { get; set; }
+            public string trainer { get; set; }
+            public int class_no { get; set; }
+            public string school { get; set; }
+            public IList<string> discipline { get; set; }
+            public IList<int> children_ages { get; set; }
+            public IList<StudentInfo> student_info { get; set; }
+        }
+
+        private class LoginResponseError
+        {
+            public string code { get; set; }
+            public string descrption { get; set; }
+        }
+
+        private class LoginResponseData
+        {
+            public string user_id { get; set; }
+        }
+
+        private class LoginResponse
+        {
+            public LoginResponseError error { get; set; }
+            public LoginResponseData data { get; set; }
+        }
+
+        private class SendRequestResponse
+        {
+            public bool status { get; set; }
+            public string message { get; set; }
+            public string url { get; set; }
+        }
+
+        private bool GetSaveFileName(ref string FileName)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.InitialDirectory = GetExePath();
+            saveFileDialog.Filter = "PDF файлы (*.pdf)|*.pdf";
+            saveFileDialog.FilterIndex = 1;
+            saveFileDialog.OverwritePrompt = true;
+            saveFileDialog.FileName = FileName;
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                FileName = saveFileDialog.FileName;
+                return true;
+            }
+            else
+                return false;
+        }
+
+        private static string GetExePath() => Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) +
+            Path.DirectorySeparatorChar;
+
+        private bool ParseList(string buffer, ref List<int> list)
+        {
+            list.Clear();
+            MatchCollection matches = new Regex(@"=(\d+)>").Matches(buffer);
+            foreach (Match match in matches)
+            {
+                int value = Convert.ToInt32(match.Groups[1].Value);
+                if (value != 0)
+                    list.Add(value);
+            }
+            return list.Count > 0;
+        }
+
+        private int RandomListValue(List<int> list) => list.Count > 0 ? list[new Random().Next(list.Count)] : 0;
+
+        private int RandomGradeByAgeId(int age_id) => age_id == 1 ? rand.Next(5, 7) : age_id == 2 ? rand.Next(8, 11) : rand.Next(1, 4);
+
+        public static ImageSource IconToImageSource(System.Drawing.Icon icon)
+        {
+            return Imaging.CreateBitmapSourceFromHIcon(
+                icon.Handle,
+                new Int32Rect(0, 0, icon.Width, icon.Height),
+                BitmapSizeOptions.FromEmptyOptions());
+        }
+
+        public class WaitCursor : IDisposable
+        {
+            private readonly Cursor _previousCursor;
+            public WaitCursor()
+            {
+                _previousCursor = Mouse.OverrideCursor;
+                Mouse.OverrideCursor = Cursors.Wait;
+            }
+            #region IDisposable Members
+            public void Dispose()
+            {
+                Mouse.OverrideCursor = _previousCursor;
+            }
+            #endregion
+        }
+    }
+}
